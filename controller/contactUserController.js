39f355bbecad5d9a1e4a891f27c2f0003d2f5109
Divyani Chhapre <divyanichhapre163@gmail.com>
2024-08-30 12:21:55 +0530

    contactuser issue resolved

--- controller/contactUserController.js
+++ controller/contactUserController.js
@@ -12,37 +12,66 @@ exports.getContactUser = async (req, res) => {
   }
 };
 
+// exports.addContactUser = async (req, res) => {
+//   const { name, email, subject, message, location } = req.body;
+//   ;
+
+//     try {
+//         const newContactUser = await new User(
+//           {
+//             name,
+//                 email,
+//                 subject,
+//                 message,
+//                 location
+//           },
+//         );
+//      res.status(200).json({ message: " user is created" });
+//       console.log(newContactUser)
+//       res.json({newContactUser})
+
+//     const newUser = new User([
+//       {
+//         name,
+//         email,
+//         subject,
+//         message,
+//       },
+//     ]);
+//     await newUser.save();
+//     res.status(200).json({ message: " user is created" },new);
+//   } catch (error) {
+//     res.status(400).json({
+//       message: "user not found",
+//     });
+//   }
+// };
 exports.addContactUser = async (req, res) => {
-  const { name, email, subject, message } = req.body;
-  const location =req.body.location;
+  const { name, email, subject, message, location } = req.body;
 
-    try {
-        const newContactUser = await User.create(
-          {
-            name,
-                email,
-                subject,
-                message,
-                location
-          },
-        );
-        res.status(200).json({ message: " user is created" });
-      console.log(newContactUser)
-      res.json({newContactUser})
+  try {
+    // Create a new user object
+    const newContactUser = new User({
+      name,
+      email,
+      subject,
+      message,
+      location,
+    });
 
-    // const newUser = new User([
-    //   {
-    //     name,
-    //     email,
-    //     subject,
-    //     message,
-    //   },
-    // ]);
-    // await newUser.save();
-    // res.status(200).json({ message: " user is created" });
+    // Save the new user to the database
+    await newContactUser.save();
+
+    // Send a single response
+    res.status(201).json({
+      message: "User is created",
+      newContactUser,
+    });
   } catch (error) {
+    // Handle any errors
     res.status(400).json({
-      message: "user not found",
+      message: "User not created",
+      error: error.message,
     });
   }
 };
