d00cac942b62106c43c93267f2f6b4306bf0edc0
Divyani Chhapre <divyanichhapre163@gmail.com>
2024-09-13 15:27:38 +0530

    volunteer photo added

--- controller/volunteerController.js
+++ controller/volunteerController.js
@@ -1,6 +1,7 @@
 const Volunteer = require("../model/volunteerSchema");
 const fs = require("fs");
 const path = require("path");
+const cloudinary = require("cloudinary").v2;
 
 exports.getVolunteer = async (req, res) => {
   try {
@@ -14,34 +15,94 @@ exports.getVolunteer = async (req, res) => {
   }
 };
 
+// exports.addVolunteer = async (req, res) => {
+//   const { name, joining, gender, age,phoneNumber, district, imgUrl } = req.body;
+
+//   try {
+//     // Create a new entry
+//     console.log("file recieved", req.files);
+//     if (!req.files || !req.files.file) {
+//       return res.status(400).json({
+//         message: "no file uploaded",
+//       });
+//     }
+
+//     const file = req.files.file;
+//     const uploadDir = path.join(__dirname, "files");
+
+//     // Create the directory if it doesn't exist
+//     if (!fs.existsSync(uploadDir)) {
+//       fs.mkdirSync(uploadDir);
+//     }
+
+//     const filePath = path.join(uploadDir, Date.now() + "_" + file.name);
+
+//     file.mv(filePath, (error) => {
+//       if (error) {
+//         return res
+//           .status(500)
+//           .json({ message: "Error saving file in volunteer", error });
+//       }
+
+//       const newVolunteer = new Volunteer({
+//         name,
+//         joining,
+//         gender,
+//         age,
+//         phoneNumber,
+//         district,
+//         imgUrl: filePath,
+//       });
+//       newVolunteer.save();
+//       res.status(201).json({
+//         message: "volunteer added successfully",
+//         newVolunteer,
+//       });
+//       console.log(newVolunteer);
+//     });
+//   } catch (error) {
+//     // Handle errors and send an appropriate response
+//     res.status(400).json({
+//       message: "Failed to add volunteer",
+//       error: error.message, // Optionally include the error message for debugging
+//     });
+//   }
+// };
+function FileTypeSupported(type, supportedType) {
+  return supportedType.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder) {
+  const options = { folder };
+
+  // options.resource_type = "auto";
+
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
 exports.addVolunteer = async (req, res) => {
-  const { name, joining, gender, age,phoneNumber, district, imgUrl } = req.body;
-
   try {
-    // Create a new entry
-    console.log("file recieved", req.files);
-    if (!req.files || !req.files.file) {
-      return res.status(400).json({
-        message: "no file uploaded",
-      });
+    const { name, joining, gender, age, phoneNumber, district, imgUrl } =
+      req.body;
+
+    const file = req.files.filge;
+    console.log(file);
+
+    const supportedType = ["jpg", "jpeg", "png", "pdf"];
+
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log("file Type", fileType);
+
+    if (!FileTypeSupported(fileType, supportedType)) {
+      return res.status(400).json({ message: "file formate not supported" });
     }
 
-    const file = req.files.file;
-    const uploadDir = path.join(__dirname, "files");
+    //upload file to cloudinary
+    const response = await uploadFileToCloudinary(file, "ngo_photos");
+    console.log(response);
 
-    // Create the directory if it doesn't exist
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir);
-    }
-
-    const filePath = path.join(uploadDir, Date.now() + "_" + file.name);
-
-    file.mv(filePath, (error) => {
-      if (error) {
-        return res
-          .status(500)
-          .json({ message: "Error saving file in volunteer", error });
-      }
+    try {
+      console.log("Files Received ");
 
       const newVolunteer = new Volunteer({
         name,
@@ -50,53 +111,69 @@ exports.addVolunteer = async (req, res) => {
         age,
         phoneNumber,
         district,
-        imgUrl: filePath,
+        imgUrl: response.secure_url,
       });
+
       newVolunteer.save();
-      res.status(201).json({
-        message: "volunteer added successfully",
-        newVolunteer,
-      });
-      console.log(newVolunteer);
+      return res
+        .status(200)
+        .json({ message: "volunteer added successfully", newVolunteer });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("server error- " + error);
+    }
+    //db me entry
+    const fileData = await Volunteer.create({
+      name,
+      joining,
+      gender,
+      age,
+      phoneNumber,
+      district,
+      imgUrl: response.secure_url,
+    });
+
+    res.status(200).json({
+      message: "Image successfully uploaded at",
+      imgUrl: response.secure_url,
     });
   } catch (error) {
-    // Handle errors and send an appropriate response
+    console.error(error);
     res.status(400).json({
+      success: false,
       message: "Failed to add volunteer",
-      error: error.message, // Optionally include the error message for debugging
     });
   }
 };
 
 exports.deleteVolunteer = async (req, res) => {
-  
   try {
-    const id = req.params.id 
-    console.log(id)
+    const id = req.params.id;
+    console.log(id);
 
     if (!id) {
       return res.status(400).json({
-       msg:"id not found"
-      })
-      
+        msg: "id not found",
+      });
     }
     const volunteer = await Volunteer.findByIdAndDelete(id);
     if (!volunteer) {
       return res.status(400).json({
-        msg:"volunteer not found"
-      })
+        msg: "volunteer not found",
+      });
     }
-    return res.status(200).json({msg:"volunteer deleted successfully",volunteer})
+    return res
+      .status(200)
+      .json({ msg: "volunteer deleted successfully", volunteer });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({message:error.message})
+    console.log(error);
+    return res.status(500).json({ message: error.message });
   }
-}
-
+};
 
 exports.searchVolunteer = async (req, res) => {
   try {
-    const {district} = req.query
+    const { district } = req.query;
 
     if (!district) {
       return res.status(400).json({ message: "District is required" });
@@ -110,10 +187,8 @@ exports.searchVolunteer = async (req, res) => {
     }
 
     res.json(volunteers);
-
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ message: "Server error" });
   }
-  
-}
+};
